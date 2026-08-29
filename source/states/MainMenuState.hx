6b1245bd1cd37a4c347217217ed13d634c20446b
Mark-bitde <mark.pozmogov@gmail.com>
2026-08-29 15:32:46 +0500

    Initial commit

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -14,7 +14,7 @@ enum MainMenuColumn {
 
 class MainMenuState extends MusicBeatState
 {
-	public static var engineVersion:String = '1.1.3';
+	public static var engineVersion:String = '1.1.4';
 	public static var psychEngineVersion:String = '1.0.4'; // This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 	public static var curColumn:MainMenuColumn = CENTER;
